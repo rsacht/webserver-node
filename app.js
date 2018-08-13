76de62fe53b76375e8652c16afbbaae4e48832c5
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-13 14:34:42 -0300

    Criação do Servidor Node

--- app.js
+++ app.js
@@ -3,5 +3,14 @@ const http = require('http');
 
 //Criação do servidor http
 http.createServer( (req, res) => {
-    res.write('Hola Mundo');
-});
+    res.write('Olá Mundo!');
+    //Precisamos dizer que já terminamos a resposta 
+    res.end();
+})
+//Configuração da porta que o servidor irá escutar
+.listen(8080);
+
+console.log('Escutando na porta 8080. Acesse: localhost:8080');
+//Teste a subida do servidor executando o comando: node app
+//Acesse o navegador e acesse: localhost:8080
+//Saída: Olá Mundo!
