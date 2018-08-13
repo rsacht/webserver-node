6b7442f1a6e2cb24b69a1c9d2f1f6a1c39f1a0b9
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-13 17:30:43 -0300

    Configurações iniciais do Express

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-var express = require('express')
-var app = express()
+const express = require('express')
+const app = express()
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
+app.get('/', (req, res) => {
+  //res.send('Olá Mundo!')
+   let saida = {
+        nome: 'Rodrigo',
+        idade: 35,
+        url: req.url
+    }
+
+    res.send(saida);
+    //Observe agora o Headers no Postman que a saída é JSON
+    //e X-Powered-By-> Express
 })
- 
 app.listen(3000)
