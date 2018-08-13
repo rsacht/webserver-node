5c81ed9a5beee35c449deca7cc803430f379cf61
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-13 14:49:25 -0300

    Transformando a resposta do servidor em conteúdo JSON

--- app.js
+++ app.js
@@ -3,14 +3,25 @@ const http = require('http');
 
 //Criação do servidor http
 http.createServer( (req, res) => {
-    res.write('Olá Mundo!');
+    
+    //Definindo o retorno da aplicação em JSON
+    res.writeHead(200,{'Content-Type': 'application/json'});
+    let saida = {
+        nome: 'Rodrigo',
+        idade: 35,
+        url: req.url
+    }
+
+    res.write(JSON.stringify(saida));
+    //Teste a saída no navegador e no postman:http://localhost:8080/
+    //No Postman veja o Content-Type em Headers:application/json
+    
+    //res.write('Olá Mundo!');
     //Precisamos dizer que já terminamos a resposta 
     res.end();
 })
 //Configuração da porta que o servidor irá escutar
 .listen(8080);
 
+//Retorno caso o servidor suba corretamente
 console.log('Escutando na porta 8080. Acesse: localhost:8080');
-//Teste a subida do servidor executando o comando: node app
-//Acesse o navegador e acesse: localhost:8080
-//Saída: Olá Mundo!
