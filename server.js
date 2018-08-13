325879afbad0a6698f5b41fa8ab4d847812d5161
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-13 17:34:48 -0300

    Configuração da mensagem de subida do servidor

--- server.js
+++ server.js
@@ -13,4 +13,8 @@ app.get('/', (req, res) => {
     //Observe agora o Headers no Postman que a saída é JSON
     //e X-Powered-By-> Express
 })
-app.listen(3000)
+//Starte o server.js: nodemon server.js
+//Configurando a mensagem de retorno ao estartar o servidor:
+app.listen(3000, () => {
+    console.log('Escutando requisições na porta 3000. Acesse: http://localhost:3000/')
+});
