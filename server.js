72549ecd39074d75935d611154c0a76e23a69f44
Rodrigo Fernandes Sacht <conectarodrigo@gmail.com>
2018-08-14 13:57:59 -0300

    Configurando servidor para renderizar home.hbs

--- server.js
+++ server.js
@@ -4,24 +4,14 @@ const app = express()
 app.use(express.static(__dirname + '/public'));
 
 //Express HBS engine 
-//Mais informações em: https://www.npmjs.com/package/hbs
 app.set('view engine', 'hbs');
 
-// app.get('/', (req, res) => {
-//   //res.send('Olá Mundo!')
-//    let saida = {
-//         nome: 'Rodrigo',
-//         idade: 35,
-//         url: req.url
-//     }
+app.get('/', (req, res) => {
 
-//     res.send(saida);
-//     //Observe agora o Headers no Postman que a saída é JSON
-//     //e X-Powered-By-> Express
-// });
-//Starte o server.js: nodemon server.js
+    res.render('home');
 
-//Configurando a mensagem de retorno ao subir o servidor:
+});
+
 app.listen(3000, () => {
     console.log('Escutando requisições na porta 3000. Acesse: http://localhost:3000/')
 });
